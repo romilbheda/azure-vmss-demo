130f5f650dda50ef54ed5b26ee45dd3357b9a086
Romil Bheda <17021364+romilbheda@users.noreply.github.com>
2019-03-13 22:52:48 +0530

    sampleapp.ps1 & sampleapp-v2.ps1 added

---
+++ sampleapp-v2.ps1
@@ -0,0 +1,2 @@
+﻿Add-WindowsFeature Web-Server
+Set-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "Sample webapp v2 running on $($env:computername) !!!"
---
+++ sampleapp.ps1
@@ -0,0 +1,2 @@
+﻿Add-WindowsFeature Web-Server
+Set-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "Sample webapp running on $($env:computername) !!!"
